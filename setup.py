e17f33256c377159f110cc148be6c2dd33e997cf
lethe <li.yi@56qq.com>
2019-08-12 15:14:06 +0800

    update url to real github project

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Simple TUI to manage docker images",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/lethe3000/v4docker",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
